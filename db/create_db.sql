60cf7c58c2370f7def3a2d980aabf5c503f7570c
Kevin Fallas <kf.ma@estudiantec.cr>
2021-12-27 23:42:57 -0600

    Add db script
    Create tables
    Insert dummy data

--- db/create_db.sql
+++ db/create_db.sql
@@ -3,35 +3,50 @@
 -- ****************************** --
 --            TABLES              --
 -- ****************************** --
-
-CREATE SCHEMA IF NOT EXISTS `inclusive_whiteboard1` ;
-
 CREATE TABLE IF NOT EXISTS inclusive_whiteboard.users (
-  id_user INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	user VARCHAR(50) NOT NULL,
 	password VARCHAR(200) NOT NULL
-)AUTO_INCREMENT=1;
+) AUTO_INCREMENT=1;
+
+
+-- ALTER TABLE inclusive_whiteboard.users AUTO_INCREMENT=1;
+-- INSERT INTO inclusive_whiteboard.users (user, password) VALUES ("kevin", "1234");
+-- INSERT INTO inclusive_whiteboard.users (user, password) VALUES ("andres", "1234");
+-- SELECT id, user FROM inclusive_whiteboard.users;
 
 CREATE TABLE IF NOT EXISTS inclusive_whiteboard.workflows (
-	id_workflow INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	id_user INT(11) NOT NULL,
-	workflow_name VARCHAR(30) NOT NULL,
-	workflow_description VARCHAR(255) NOT NULL,
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	user_id INT NOT NULL,
+	name VARCHAR(30) NOT NULL,
+	description VARCHAR(255) NOT NULL,
 	creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	CONSTRAINT FOREIGN KEY fk_workflows_users (id_user) REFERENCES users (id_user)
-)AUTO_INCREMENT=1;
+	CONSTRAINT FOREIGN KEY fk_workflows_users (user_id) REFERENCES users (id)
+) AUTO_INCREMENT=1;
+
+-- INSERT INTO inclusive_whiteboard.workflows (user_id, name, description) VALUES (1, "Tabla 1", "Proyectos verano");
+-- INSERT INTO inclusive_whiteboard.workflows (user_id, name, description) VALUES (1, "Tabla 2", "Proyectos semestre 1 2022");
+-- INSERT INTO inclusive_whiteboard.workflows (user_id, name, description) VALUES (2, "1234", "Semestre 1 2019");
+
+-- SELECT id, user_id, name, description, creation_date FROM inclusive_whiteboard.workflows WHERE user_id = 2;
 
 CREATE TABLE IF NOT EXISTS inclusive_whiteboard.sticky_notes (
-	id_sticky_note INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	id_user INT(11) DEFAULT NULL,
-	id_workflow INT(11) DEFAULT NULL,
-	status INT(1) NOT NULL,
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	user_id INT DEFAULT NULL,
+	workflow_id INT DEFAULT NULL,
+	status VARCHAR(50) NOT NULL,
 	description VARCHAR(255) DEFAULT NULL,
 	color VARCHAR(7) DEFAULT NULL,
-	CONSTRAINT FOREIGN KEY fk_stickynotes_users (id_user) REFERENCES users (id_user),
-	CONSTRAINT FOREIGN KEY fk_stickynotes_workflow (id_workflow) REFERENCES workflows (id_workflow)
-)AUTO_INCREMENT=1;
+	CONSTRAINT FOREIGN KEY fk_stickynotes_users (user_id) REFERENCES users (id),
+	CONSTRAINT FOREIGN KEY fk_stickynotes_workflow (workflow_id) REFERENCES workflows (id)
+) AUTO_INCREMENT=1 ;
+  
+-- INSERT INTO inclusive_whiteboard.sticky_notes (
+-- 	user_id,
+-- 	workflow_id,
+-- 	status,
+-- 	description,
+-- 	color
+-- ) VALUES (1, 1, "In progress", "Texto sticky note", "#4676a3");
 
-
-
-
+  
