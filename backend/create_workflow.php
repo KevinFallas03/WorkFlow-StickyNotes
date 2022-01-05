d91f9fcc232c1114292e5e26d55d0ce8617bea61
Kevin Fallas <kf.ma@estudiantec.cr>
2022-01-05 08:03:16 -0600

    Create create_workflow.php template

---
+++ backend/create_workflow.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+
+//require "control_sesion.php"; //importa el control de sesiones el require detecta errores Fatales en la ejecución del archivo importado no así el include!
+include "mysqli_connection.php";
+
+/*********Eliminar estando producción************/
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+ini_set("display_errors", 1);
+/*************************************************/
+
+if (!isset($_SESSION["user_id"]))
+{
+    echo ("{'error':'log in needed'}");
+    exit();
+}
+
+$user_id=$_SESSION["user_id"];
+// $body = 
+
+$conn = get_connection();
+$query = "  INSERT INTO workflows
+            (
+                  user_id
+                , name
+                , description
+            )
+            VALUES
+            (
+
+            )
+                 
+            FROM workflows 
+            WHERE user_id='$user_id' ";
+
+$workflows = run_query(
+    $conn, 
+    $query
+);
+
+
+$json = mysqli_fetch_all ($workflows, MYSQLI_ASSOC);
+echo json_encode($json);
+
+mysqli_close($conn);
+?>
