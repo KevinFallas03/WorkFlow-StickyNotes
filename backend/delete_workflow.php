e25c2fa2fab4ff520c8af48045e725116b0a0985
Kevin Fallas <kf.ma@estudiantec.cr>
2022-01-05 07:37:54 -0600

    Fix bug on delete in backend

--- backend/delete_workflow.php
+++ backend/delete_workflow.php
@@ -1,5 +1,5 @@
 <?php
-// session_start();
+session_start();
 
 include "mysqli_connection.php";
 
@@ -8,12 +8,12 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE);
 ini_set("display_errors", 1);
 /*************************************************/
 
-if (!isset($_REQUEST["user_id"]))
+if (!isset($_SESSION["user_id"]))
 {
     echo ("{'error':'login needed'}");
     exit();
 }
-$user_id = $_REQUEST["user_id"];
+$user_id = $_SESSION["user_id"];
 
 if (!isset($_REQUEST["workflow_id"]))
 {
@@ -23,21 +23,19 @@ if (!isset($_REQUEST["workflow_id"]))
 $workflow_id = $_REQUEST["workflow_id"]; 
 
 $conn = get_connection();
+$query = "  DELETE FROM workflows
+            WHERE id = '$workflow_id' AND user_id = '$user_id'; ";
 
 $result = run_query(
     $conn, 
-    "   
-        DELETE FROM workflows 
-        WHERE id = '$user_id' AND user_id = '$workflow_id';
-
-    "
+    $query
 );
+$rows_affected = $conn->affected_rows;
 
-if($result){
-    echo "Workflow Deleted";
-}
-else{
-    echo "Workflow Not Deleted";
+if($rows_affected > 0){
+    echo "[true, {'deleted': '$workflow_id'}]";
+}else{
+    echo "[false, {'error': 'no rows affected'}]";
 }
 
 mysqli_close($conn);
