a073a5fe0cb29e29a5e70bd88804169ecef6bb3e
Esteban Madrigal <ememadrigal.em@gmail.com>
2022-01-05 19:43:41 -0600

    change id per class on html

--- scripts/workflows.js
+++ scripts/workflows.js
@@ -1,47 +1,40 @@
 var data = [{}]
 
-window.onload = () =>{
+window.onload = () => {
     get_workflows();
 }
 
-function workflows_request()
-{
+function workflows_request() {
     var url = "/backend/workflows/get_workflows.php"
     var xhttp = new XMLHttpRequest();
 
-    xhttp.onreadystatechange = function() 
-    {
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) 
-        {
-            response = eval ("("+xhttp.responseText+")");
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            response = eval("(" + xhttp.responseText + ")");
             console.log(response);
-            if (response[0]==false)
-            {
+            if (response[0] == false) {
                 console.log(response[0].error);
             }
-            else
-            {
+            else {
                 data = response;
             }
         }
         else {
-            console.log({"status": this.status, "state": this.readyState})
+            console.log({ "status": this.status, "state": this.readyState })
         }
     };
     xhttp.open("GET", url, false);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send();  
+    xhttp.send();
 }
 
-function get_workflows()
-{
+function get_workflows() {
     data = [{}];
     workflows_request();
     data.forEach(
-        element => 
-        {
+        element => {
             create_workflow_on_httml(element);
-        }   
+        }
     )
 }
 
@@ -52,34 +45,34 @@ function create_workflow_on_httml(element) {
     workflow.setAttribute("name", "workflow" + element.id);
 
     delete_button = document.createElement("div");
-    delete_button.setAttribute("id", "delete_button" + element.id); 
+    delete_button.setAttribute("id", "delete_button" + element.id);
     delete_button.className = "crud-btn";
     delete_icon = document.createElement("i");
-    delete_icon.className = "fa fa-trash"; 
+    delete_icon.className = "fa fa-trash";
     delete_button.appendChild(delete_icon);
-    
+
     edit_button = document.createElement("div");
-    edit_button.setAttribute("id", "edit_button" + element.id); 
+    edit_button.setAttribute("id", "edit_button" + element.id);
     edit_button.className = "crud-btn";
     edit_icon = document.createElement("i");
-    edit_icon.className = "fas fa-pencil-alt"; 
+    edit_icon.className = "fas fa-pencil-alt";
     edit_button.appendChild(edit_icon);
-    
+
     info_button = document.createElement("div");
-    info_button.setAttribute("id", "info_button" + element.id); 
+    info_button.setAttribute("id", "info_button" + element.id);
     info_button.className = "crud-btn";
     info_icon = document.createElement("i");
     info_icon.className = "fa fa-info-circle";
     info_button.appendChild(info_icon);
 
     workflow_clickable = document.createElement("div");
-    workflow_clickable.setAttribute("id", "wf_clickable" + element.id); 
+    workflow_clickable.setAttribute("id", "wf_clickable" + element.id);
     workflow_clickable.className = "wf-clickable";
     workflow_clickable.innerHTML = element.name;
 
     delete_button.onclick = () => {
         // alert("Deleting workflow " + element.name + " id:" + element.id);
-        workflow_to_delete = document.getElementsByName("workflow"+element.id);
+        workflow_to_delete = document.getElementsByName("workflow" + element.id);
         // console.log(element.id);
         delete_workflow(workflow_to_delete[0]); // always in 0 because the element returned is unique
     };
@@ -94,8 +87,8 @@ function create_workflow_on_httml(element) {
     };
 
     info_button.onclick = () => {
-        alert("Workflow "+ element.id +": " + element.name 
-            +"\nDescription: " + element.description 
+        alert("Workflow " + element.id + ": " + element.name
+            + "\nDescription: " + element.description
             + "\nCreation date: " + element.creation_date);
     };
 
@@ -103,7 +96,7 @@ function create_workflow_on_httml(element) {
         alert("Switching to workflow " + element.name);
         window.localStorage.setItem("currentWorkflow", element.id);
     }
-    
+
     workflow.appendChild(workflow_clickable);
     workflow.appendChild(delete_button);
     workflow.appendChild(edit_button);
@@ -112,7 +105,7 @@ function create_workflow_on_httml(element) {
     document.getElementById("workflow-list-id").appendChild(workflow);
 }
 
-function edit_workflow_data(workflow_id, name, description){
+function edit_workflow_data(workflow_id, name, description) {
     var url = "/backend/workflows/edit_workflow_data.php";
     var params = new FormData();
     params.append("workflow_id", workflow_id);
@@ -122,41 +115,40 @@ function edit_workflow_data(workflow_id, name, description){
     xhttp.open("POST", url, false);
     // xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) 
-        {
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
             // response = eval ("("+xhttp.responseText+")");
             console.log(response);
-            if (response[0] == false){
+            if (response[0] == false) {
                 console.log(response[0].error);
             } else {
                 window.location = "index.html";
             }
         }
         else {
-            console.log({"status": this.status, "state": this.readyState})
+            console.log({ "status": this.status, "state": this.readyState })
         }
     };
 
-    xhttp.send(params);  
+    xhttp.send(params);
 }
 
-function add_states(){
-
-}
-
-function new_workflow(){
+function add_states() {
 
 }
 
-function post_workflow(name, description){
+function new_workflow() {
+
+}
+
+function post_workflow(name, description) {
     var url = "/backend/workflows/create_workflow.php"
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
+    xhttp.onreadystatechange = function () {
         if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
             console.log(xhttp.responseText);
             // response=eval ("("+xhttp.responseText+")");
-            
+
             // if (response[0]==false){
             //     console.log(response[1].error);
             // }
@@ -165,7 +157,7 @@ function post_workflow(name, description){
             // }
         }
         else {
-            console.log({'status': this.status, 'state': this.readyState})
+            console.log({ 'status': this.status, 'state': this.readyState })
         }
     };
     xhttp.open("POST", url, false);
@@ -178,21 +170,21 @@ function post_workflow(name, description){
 
     var str_json = "json_string=" + (JSON.stringify(parameters));
     xhttp.send(str_json);
-} 
+}
 
-function delete_workflow(workflow){
+function delete_workflow(workflow) {
     var url = "/backend/workflows/delete_workflow.php";
-    
+
     var params = new FormData();
     params.append("workflow_id", workflow.id);
-    
+
     var xhttp = new XMLHttpRequest();
     xhttp.open("POST", url);
     // xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhttp.send(params);
 
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {            
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
             // response = eval ("("+xhttp.responseText+")");
             if (response[0] == false) {
                 console.log(response[0].error);
@@ -200,72 +192,100 @@ function delete_workflow(workflow){
                 window.location = "index.html";
             }
         } else {
-            console.log({"status": this.status, "state": this.readyState})
+            console.log({ "status": this.status, "state": this.readyState })
         }
     };
 }
 
-function move_right(id){
-    var header = document.getElementById("th"+id);
-    var column = document.getElementById("td"+id);
+function move_right(id) {
+    var header = document.getElementsByClassName("state_" + id)[0];
+    var column = document.getElementsByClassName("state_body_" + id)[0];
+    console.log(column);
 
     var headers = document.getElementById('workflow_headers');
     var body = document.getElementById('workflow_states');
 
     for (let i = 0; i < column.children.length; i++) {
-        var left = parseInt(column.children[i].style.left.slice(0,-2));
-        column.children[i].style.left = (left+120)+'px';
+        var left = parseInt(column.children[i].style.left.slice(0, -2));
+        column.children[i].style.left = (left + 120) + 'px';
     }
 
     for (let i = 0; i < column.nextElementSibling.children.length; i++) {
-        var left = parseInt(column.nextElementSibling.children[i].style.left.slice(0,-2));
-        column.nextElementSibling.children[i].style.left = (left-120)+'px';
+        var left = parseInt(column.nextElementSibling.children[i].style.left.slice(0, -2));
+        column.nextElementSibling.children[i].style.left = (left - 120) + 'px';
     }
 
-    headers.insertBefore(header,header.nextElementSibling.nextElementSibling);
-    body.insertBefore(column,column.nextElementSibling.nextElementSibling);
+    headers.insertBefore(header, header.nextElementSibling.nextElementSibling);
+    body.insertBefore(column, column.nextElementSibling.nextElementSibling);
 }
 
-function move_left(id){
-    var header = document.getElementById("th"+id);
-    var column = document.getElementById("td"+id);
+function move_left(id) {
+    var header = document.getElementsByClassName("state_" + id)[0];
+    var column = document.getElementsByClassName("state_body_" + id)[0];
 
     var headers = document.getElementById('workflow_headers');
     var body = document.getElementById('workflow_states');
 
-    headers.insertBefore(header,header.previousElementSibling);
-    body.insertBefore(column,column.previousElementSibling);
+    headers.insertBefore(header, header.previousElementSibling);
+    body.insertBefore(column, column.previousElementSibling);
 
     for (let i = 0; i < column.children.length; i++) {
-        var left = parseInt(column.children[i].style.left.slice(0,-2));
-        column.children[i].style.left = (left-120)+'px';
+        var left = parseInt(column.children[i].style.left.slice(0, -2));
+        column.children[i].style.left = (left - 120) + 'px';
     }
 
     for (let i = 0; i < column.nextElementSibling.children.length; i++) {
-        var left = parseInt(column.nextElementSibling.children[i].style.left.slice(0,-2));
-        column.nextElementSibling.children[i].style.left = (left+120)+'px';
+        var left = parseInt(column.nextElementSibling.children[i].style.left.slice(0, -2));
+        column.nextElementSibling.children[i].style.left = (left + 120) + 'px';
     }
 }
 
-function delete_status(id){
-    var header = document.getElementById("th"+id);
-    var column = document.getElementById("td"+id);
+function delete_status(id) {
+    var header = document.getElementsByClassName("state_" + id)[0];
+    var column = document.getElementsByClassName("state_body_" + id)[0];
     header.remove();
     column.remove();
 }
 
-function create_status(id){
-    var header = document.getElementById("th"+id);
-    var column = document.getElementById("td"+id);
+function create_status(id) {
+    var header = document.getElementsByClassName("state_" + id)[0];
+    var column = document.getElementsByClassName("state_body_" + id)[0];
 
     var headers = document.getElementById('workflow_headers');
     var body = document.getElementById('workflow_states');
 
-    var new_state = prompt("Please enter your name", "New state");
+    var new_state = prompt("Please enter the new state name", "New state");
 
     var new_header = header.cloneNode(true);
     var new_column = document.createElement("td");
     new_header.textContent = new_state;
-    headers.insertBefore(new_header,header.nextElementSibling);
-    body.insertBefore(new_column,column.nextElementSibling);
+    headers.insertBefore(new_header, header.nextElementSibling);
+    body.insertBefore(new_column, column.nextElementSibling);
+
+    var url = "/backend/states/insert_state.php"
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            if (xhttp.responseText < 0) { // If is negative, there was an error
+                alert("Error: State not saved");
+            }
+            else {
+                elmnt.id = "sticky_note_" + xhttp.responseText;
+            }
+        }
+        else {
+            console.log({ 'status': this.status, 'state': this.readyState })
+        }
+    };
+    xhttp.open("POST", url, false);
+    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    var parameters = {
+        'workflow_id': 14,
+        'name': new_state,
+        'position': elmnt.outerHTML
+    };
+
+    var str_json = "json_string=" + (JSON.stringify(parameters));
+    xhttp.send(str_json);
 }
