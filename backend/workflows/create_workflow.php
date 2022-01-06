c50bd2411b2bad663d9dd2267a94661208c86c33
Kevin Fallas <kf.ma@estudiantec.cr>
2022-01-05 18:21:59 -0600

    Finish create workflow backend

--- backend/workflows/create_workflow.php
+++ backend/workflows/create_workflow.php
@@ -2,7 +2,7 @@
 session_start();
 
 //require "control_sesion.php"; //importa el control de sesiones el require detecta errores Fatales en la ejecución del archivo importado no así el include!
-include "mysqli_connection.php";
+include "../mysqli_connection.php";
 
 /*********Eliminar estando producción************/
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
@@ -16,38 +16,110 @@ if (!isset($_SESSION["user_id"]))
 }
 $user_id=$_SESSION["user_id"];
 
-if (!isset($_POST['json_string']))
+if (!isset($_REQUEST['name']))
 {
-    echo ("{'error':'workflow_id parameters is needed'}");
+    echo ("{'error':'name parameter is needed'}");
+    exit();
+}
+$name = $_REQUEST['name'];
+
+if (!isset($_REQUEST['description']))
+{
+    echo ("{'error':'description parameter is needed'}");
+    exit();
+}
+$description = $_REQUEST['description'];
+
+echo "$name";
+echo "$description";
+
+$conn = get_connection();
+
+$insert_wf_query = "INSERT INTO workflows
+                    (
+                        user_id
+                        , name
+                        , description
+                    )
+                    VALUES
+                    (
+                        $user_id
+                        , '$name'
+                        , '$description'
+                    );";
+
+$insert_result = run_query(
+    $conn, 
+    $insert_wf_query
+);
+
+if($conn->affected_rows <= 0) {
+    echo "[false, {'error': 'could not insert the workflow'}]";
     exit();
 }
 
-$body = $_POST['json_string'];
-$data = json_decode($body);
+$workflow_id = $conn->insert_id;
 
-$conn = get_connection();
-$query = "  INSERT INTO workflows
-            (
-                  user_id
-                , name
-                , description
-            )
-            VALUES
-            (
-                  $user_id
-                , $data->name
-                , $data->description
-            )
-        ";
-
-$workflows = run_query(
-    $conn, 
-    $query
+$queries = array(
+    "  INSERT INTO states
+        (
+            user_id
+            , workflow_id
+            , name
+            , position
+        )
+        VALUES
+        (
+            $user_id
+            , $workflow_id
+            , 'Unstarted'
+            , 1
+        );
+    ",
+    "  INSERT INTO states
+        (
+            user_id
+            , workflow_id
+            , name
+            , position
+        )
+        VALUES
+        (
+            $user_id
+            , $workflow_id
+            , 'Started'
+            , 2
+        );
+    ",
+    "  INSERT INTO states
+        (
+            user_id
+            , workflow_id
+            , name
+            , position
+        )
+        VALUES
+        (
+            $user_id
+            , $workflow_id
+            , 'Finised'
+            , 3
+        );
+    "
 );
 
+foreach($queries as $query){
+    $result = run_query(
+        $conn, 
+        $query
+    );
+}
 
-$json = mysqli_fetch_all ($workflows, MYSQLI_ASSOC);
-echo json_encode($json);
+if($conn->affected_rows > 0){
+    echo "[true, {'success': 'workflow created'}]";
+} else {
+    echo "[false, {'error': 'states did not created'}]";
+}
 
 mysqli_close($conn);
 ?>
